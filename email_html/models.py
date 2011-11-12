460f30c408b9a32e587f4ded32e6d01308cbe796
ram <ramusus@gmail.com>
2011-11-13 00:53:55 +0400

    added monkeypatching for User.send_email method

--- email_html/models.py
+++ email_html/models.py
@@ -1,9 +1,18 @@
 # substitute send_mail function.
 from django.conf import settings
+from django.contrib.auth.models import User
 from mail import send_mail as send_html_mail
 
 from django.core import mail
 mail.send_mail = send_html_mail
+
+def email_user(self, subject, message, from_email=None):
+     """
+     Sends an email to this User.
+     """
+     send_html_mail(subject, message, from_email, [self.email])
+
+User.email_user = email_user
 
 if 'mailer' in settings.INSTALLED_APPS:
     import mailer
