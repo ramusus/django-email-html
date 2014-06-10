36f4715c547342da49f076718189a9591318248e
ramusus <ramusus@gmail.com>
2014-06-11 00:38:36 +0400

    fixed issue #2

--- email_html/models.py
+++ email_html/models.py
@@ -1,10 +1,17 @@
 # substitute send_mail function.
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core import mail
 from .mail import send_mail as send_html_mail
 
-from django.core import mail
 mail.send_mail = send_html_mail
+
+# https://docs.djangoproject.com/en/dev/topics/auth/customizing/#referencing-the-user-model
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except ImportError:
+    pass
 
 def email_user(self, subject, message, from_email=None):
      """
